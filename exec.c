2b09ace643a1d3363e0c53f2e15d706ac13fdec7
Mokriy Yuriy <mavrandr@mail.ru>
2015-11-08 20:06:30 +0300

    exec improved: exec can execute scipts

--- exec.c
+++ exec.c
@@ -7,8 +7,18 @@
 #include "x86.h"
 #include "elf.h"
 
+int cleverexec(char *, char **, int);
+char checkinterpreter(struct inode *);
+int scriptexec(struct inode *, char *, char **, int);
+
 int
 exec(char *path, char **argv)
+{
+  return cleverexec(path, argv, 5);
+}
+
+int
+cleverexec(char *path, char **argv, int recursion_limit)
 {
   char *s, *last;
   int i, off;
@@ -17,6 +27,10 @@ exec(char *path, char **argv)
   struct inode *ip;
   struct proghdr ph;
   pde_t *pgdir, *oldpgdir;
+
+  if(!recursion_limit) {
+    return -1;
+  }
 
   begin_op();
   if((ip = namei(path)) == 0){
@@ -28,9 +42,9 @@ exec(char *path, char **argv)
 
   // Check ELF header
   if(readi(ip, (char*)&elf, 0, sizeof(elf)) < sizeof(elf))
-    goto bad;
+    goto script;
   if(elf.magic != ELF_MAGIC)
-    goto bad;
+    goto script;
 
   if((pgdir = setupkvm()) == 0)
     goto bad;
@@ -103,5 +117,54 @@ exec(char *path, char **argv)
     iunlockput(ip);
     end_op();
   }
+  return -1;
+
+ script:
+  return scriptexec(ip, path, argv, recursion_limit);
+}
+
+char
+checkinterpreter(struct inode *ip)
+{
+  char fstr[2];
+  int ln = readi(ip, fstr, 0, 2);
+  return ln >= 2 && fstr[0] == '#' && fstr[1] == '!'; 
+}
+
+int
+scriptexec(struct inode *ip, char *pathname, char **argv, int recursion_limit)
+{
+  char *interpreter_path;
+  int ln, argc, i, res, size;
+  if(!checkinterpreter(ip)) {
+    goto bad;
+  }
+  interpreter_path = kalloc();
+  size = readi(ip, interpreter_path, 2, 4000);
+  iunlockput(ip);
+  end_op();
+  for(ln = 0; ln < size && interpreter_path[ln] != '\n'; ln++) {}
+  if(ln == size) {
+    goto bad;
+  }
+  interpreter_path[ln] = 0;
+  for(argc = 0; argv[argc]; argc++) {}
+  if(argc + 1 >= MAXARG) {
+    goto bad;
+  };
+  for(i = argc; i >= 0; i--) {
+    argv[i + 1] = argv[i];
+  }
+  argv[0] = interpreter_path;
+  pathname = interpreter_path;
+  res = cleverexec(pathname, argv, recursion_limit - 1);
+  kfree(interpreter_path);
+  return res;
+ bad:
+  if(ip) {
+    iunlockput(ip);
+    end_op();
+  }
+  kfree(interpreter_path);
   return -1;
 }
